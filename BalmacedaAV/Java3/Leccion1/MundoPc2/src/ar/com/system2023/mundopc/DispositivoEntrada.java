4e271ed66655e24e0a7d01072f8839fb1c114f83
AVBalmaceda <aadriveross@gmail.com>
2024-04-10 23:46:22 -0300

    Leccion1 en Java

---
+++ BalmacedaAV/Java3/Leccion1/MundoPc2/src/ar/com/system2023/mundopc/DispositivoEntrada.java
@@ -0,0 +1,41 @@
+/*
+ * Java3
+Leccion1
+ */
+package ar.com.system2023.mundopc;
+
+/**
+ *
+ * @author Adriana
+ */
+public class DispositivoEntrada {
+    private String tipoEntrada;
+    private String marca;
+    
+    public DispositivoEntrada(String tipoEntrada, String marca){
+        this.tipoEntrada = tipoEntrada;
+        this.marca = marca;
+    }
+
+    public String getTipoEntrada() {
+        return tipoEntrada;
+    }
+
+    public void setTipoEntrada(String tipoEntrada) {
+        this.tipoEntrada = tipoEntrada;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "DispositivoEntrada{" + "tipoEntrada=" + tipoEntrada + ", marca=" + marca + '}';
+    }
+    
+}
