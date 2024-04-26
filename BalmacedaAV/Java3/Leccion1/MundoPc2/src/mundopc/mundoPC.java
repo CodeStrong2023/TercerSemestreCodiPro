c620a68b5fcf88550e731c0a5ac6e1dc3ac0e0e3
AVBalmaceda <aadriveross@gmail.com>
2024-04-25 21:54:37 -0300

    mundoPC/Computadora

---
+++ BalmacedaAV/Java3/Leccion1/MundoPc2/src/mundopc/mundoPC.java
@@ -0,0 +1,39 @@
+/*
+ * MundoPc
+ */
+package mundopc;
+
+import ar.com.system2023.mundopc.*;
+
+/**
+ * @author Adriana
+ */
+public class mundoPC {
+    public static void main(String[] args){
+        
+        Monitor monitorHP = new Monitor("HP", 13); //Importar la clase
+        Teclado tecladoHP = new Teclado("Bluetooth", "HP");
+        Raton ratonHP = new Raton("Bluetooth", "HP"); 
+        Computadora computadoraHP = new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
+        
+        //Crear otros objetos de diferente marca
+        Monitor monitorGamer = new Monitor("Gamer", 32); 
+        Teclado tecladoGamer = new Teclado("Bluetooth", "Gamer");
+        Raton ratonGamer = new Raton("Bluetooth", "Gamer"); 
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+        Orden orden1 = new Orden(); //Inicializar el arreglo vacio
+        Orden orden2 = new Orden(); //Nueva lista para el objeto orden2
+        orden1.agregarComputadora(computadoraHP);
+        orden1.agregarComputadora(computadoraGamer);
+        
+        Computadora computadorasVarias = new Computadora("Computadoras de diferentes marcas", monitorHP, tecladoGamer, ratonHP);
+        orden2.agregarComputadora(computadorasVarias);
+        
+        orden1.mostrarOrden();
+        orden2.mostrarOrden();
+        
+        //Crear mas objetos de tipo computadora con todos sus elementos
+        //Completar una lista en el objeto orden1 que llegue a los 10 elementos
+        //Probar de esta manera los metodos al maximo rendimiento
+    }
+}
