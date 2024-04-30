68af3df8b879be7f7dac95fbf7bbe25a92203e98
Siro01 <sirooo444@gmail.com>
2024-04-30 19:18:28 -0300

    Clase 3 de Laboratorio III en Python
    Primera parte de la clase 3 en Python, sobrecarga de operadores y sobreescitura

---
+++ SiroTorres/Python/Leccion12/sobrecarga_operadores.py
@@ -0,0 +1,18 @@
+
+a = 3
+b = 5
+print(a + b)
+
+a = 'Hola'
+b = ' mundo'
+
+print(a + b)
+
+a = [3, 4, 5]
+b = [6, 7, 8, 9]
+print(a + b)
+
+# miObjeto1 + miObjeto2 = esto no se podria hacer
+#Se necesita una sobrecarga para que el + cumpla su funcion
+
+
