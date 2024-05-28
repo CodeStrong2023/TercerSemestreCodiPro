abea272e6c3ce24ab05f6ab323021a117a471c96
AVBalmaceda <aadriveross@gmail.com>
2024-05-28 17:06:31 -0300

    Repaso Laboratorio3 y Java3

---
+++ BalmacedaAV/Java3/Leccion3/ForEach/src/domain/Persona.java
@@ -0,0 +1,31 @@
+/*
+ * ForEach
+ */
+package domain;
+
+/*
+ * @author Adriana
+ */
+public class Persona {
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre = " + nombre + '}';
+    }
+    
+    
+    
+}
