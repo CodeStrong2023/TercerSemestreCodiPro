095ce54e4a63cfbd48f3c7e6da1adc5d6366dc97
Siro01 <sirooo444@gmail.com>
2024-06-20 19:11:34 -0300

    Clase 11 de Java
    Proyecto estudiantes Parte 1

---
+++ SiroTorres/Java/SQL/Conexion01.sql
@@ -0,0 +1,7 @@
+
+-- Comenzamos con CRUD: create(insertar), read(leer), update(acutalizar), delete (eliminar)
+-- Listar los estudiantes (read)
+SELECT * FROM estudiantes2024;
+-- Insertar estudiante
+INSERT INTO estudiantes2024(nombre, apellido, telefono, email) VALUES ("Juan", "Perez", "2615555555", "juan@mail.com")
+
